fe292b752a8c2cf9f5097594ab7c63fdfe13a9ca
Mat3u52 <kolodziej2007@tlen.pl>
2023-02-04 06:32:40 +0100

    UX schema.

--- desk/datasheet/views.py
+++ desk/datasheet/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Guideline
 
 
 def guideline_list(request):
-    return render(request, 'datasheet/guideline_list.html', {})
+    guideline = Guideline.objects.filter(publish_date__lte=timezone.now()).order_by('publish_date')
+    return render(request, 'datasheet/guideline_list.html', {'guideline': guideline})
