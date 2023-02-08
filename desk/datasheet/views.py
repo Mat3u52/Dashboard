2393b7cc27ce0ac7c65f911c1f7bb61cf4361591
Mat3u52 <kolodziej2007@tlen.pl>
2023-02-08 05:44:15 +0100

    Styling the website.

--- desk/datasheet/views.py
+++ desk/datasheet/views.py
@@ -1,8 +1,13 @@
+import datetime
+
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Guideline
 
 
 def guideline_list(request):
+    current_date = datetime.date.today()
     guideline = Guideline.objects.filter(publish_date__lte=timezone.now()).order_by('publish_date')
-    return render(request, 'datasheet/guideline_list.html', {'guideline': guideline})
+    return render(request, 'datasheet/guideline_list.html',
+                  {'current_date': current_date,
+                   'guideline': guideline})
