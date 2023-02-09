7a93b7041a206d488cc833f28f7d84b7d86478ef
Mat3u52 <kolodziej2007@tlen.pl>
2023-02-09 05:50:01 +0100

    Added the base.html file.

--- desk/datasheet/urls.py
+++ desk/datasheet/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.guideline_list, name='guideline_list'),
+    path('guideline/<int:pk>', views.guideline_detail, name='guideline_detail'),
 ]
--- desk/datasheet/views.py
+++ desk/datasheet/views.py
@@ -1,6 +1,6 @@
 import datetime
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Guideline
 
@@ -11,3 +11,10 @@ def guideline_list(request):
     return render(request, 'datasheet/guideline_list.html',
                   {'current_date': current_date,
                    'guideline': guideline})
+
+
+def guideline_detail(request, pk):
+    one_guideline = get_object_or_404(Guideline, pk=pk)
+    return render(request, 'datasheet/guideline_detail.html', {'one_guideline': one_guideline})
+
+
