cebdaac1d39524254d500da60775d44193460b0f
Mat3u52 <kolodziej2007@tlen.pl>
2023-04-10 07:15:48 +0200

    Added calss Comments.

--- desk/datasheet/admin.py
+++ desk/datasheet/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Guideline
+from .models import Guideline, Comment
 
 # admin.site.register(Guideline)
 
@@ -12,6 +12,13 @@ class GuideAdmin(admin.ModelAdmin):
     raw_id_fields = ('author',)
     date_hierarchy = 'publish_date'
     ordering = ('status', 'publish_date')
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'guideline', 'created', 'active')
+    list_filter = ('active', 'created', 'updated')
+    search_fields = ('name', 'email', 'body')
 
 
 
