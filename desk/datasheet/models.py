93526a340182755e13f31d1e089d2c272b05e593
Mat3u52 <kolodziej2007@tlen.pl>
2023-04-13 04:52:40 +0200

    Added models for comments.

--- desk/datasheet/models.py
+++ desk/datasheet/models.py
@@ -38,3 +38,22 @@ class Guideline(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Comment(models.Model):
+    guideline = models.ForeignKey(Guideline,
+                                  on_delete=models.CASCADE,
+                                  related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ('created',)
+
+    def __str__(self):
+        return 'The comment was added by {} for guideline {}'.format(self.name, self.guideline)
+
