535aa7dc51584e973ce14fac13684d660cb060c4
Mat3u52 <kolodziej2007@tlen.pl>
2023-04-25 05:14:44 +0200

    Selenium E2E

--- desk/datasheet/seleniumTests/TestGuidelineInit.py
+++ desk/datasheet/seleniumTests/TestGuidelineInit.py
@@ -2,7 +2,6 @@ import unittest
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-# from selenium.common.exceptions import NoSuchElementException
 from FakeDataGenerator import FakeDataGenerator
 
 
@@ -18,8 +17,6 @@ class TestGuidelineInit(unittest.TestCase):
         self.driver.implicitly_wait(2)
         self.driver.maximize_window()
         self.driver.get('http://127.0.0.1:8000/')
-
-        # raise unittest.SkipTest("testttt")
 
     def tearDown(self) -> None:
         """
@@ -38,11 +35,10 @@ class TestAbandon(TestGuidelineInit):
         :return: Webpage "desk"
         :rtype: None
         """
-        print(FakeDataGenerator().fake_name())
 
         try:
+            assert len(self.driver.window_handles) == 1
             self.assertIn('Guideline for AXI', self.driver.page_source, msg=None)
-            # self.fail('fuckup')
         except AssertionError:
             raise unittest.SkipTest("skip all tests in this class")
         time.sleep(2)
@@ -50,7 +46,7 @@ class TestAbandon(TestGuidelineInit):
 
 class TestGuidelineMain(TestGuidelineInit):
 
-    def test_admin_login_logout(self) -> None:
+    def _admin_login_logout(self) -> None:
         """
         Verify the user is login as admin or not. If not then sign in as admin.
         :return: admin panel
@@ -66,19 +62,54 @@ class TestGuidelineMain(TestGuidelineInit):
             "//div[@class='labelMain']//a[@href='/admin/']//button"
         ).click()
 
-        print(self.button_name)
         try:
             self.assertEqual(self.button_name, 'User', msg=None)
+
             self.input_username = self.driver.find_element(
                 By.XPATH,
                 "//div[@class='form-row']//input[@id='id_username']"
             )
+            self.input_password = self.driver.find_element(
+                By.XPATH,
+                "//div[@class ='form-row']//input[@id='id_password']"
+            )
+            self.submit_user_pass = self.driver.find_element(
+                By.XPATH,
+                "//div[@class ='submit-row']//input[@type='submit']"
+            )
             self.input_username.send_keys("admin")
+            self.input_password.send_keys("admin")
+            self.submit_user_pass.submit()
+            time.sleep(2)
+            self.href_to_webpage = self.driver.find_element(
+                By.XPATH,
+                "//div[@id='user-tools']//a[@href='/']"
+                # "//div[@id='user-tools']//a[1]"
+            )
+            self.href_to_webpage.click()
             time.sleep(2)
         except AssertionError:
             self.skipTest("The user name is different than \"User\" ")
 
+    def test_create_new_guideline(self) -> None:
+        """
+        The function create a new guideline by selenium.
+
+        :return: a new guideline in DB
+        :rtype: None
+        """
+        self._admin_login_logout()
+        self.button_new_guideline = self.driver.find_element(
+            By.XPATH,
+            "//div[@class='labelMain']//a[@href='/api/guideline/new/']"
+        )
+        self.button_new_guideline.click()
+        time.sleep(2)
+        print(FakeDataGenerator().fake_login())
+        # self.driver.switch_to.new_window('tab')
+        # time.sleep(2)
+
+
 
 if __name__ == "__main__":
     unittest.main(failfast=True)
-    # unittest.main(exit=False)
