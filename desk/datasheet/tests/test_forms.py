38a2ae528a8b26037b5deaf53d6c8b32b092c5a3
Mat3u52 <kolodziej2007@tlen.pl>
2023-05-02 08:37:06 +0200

    type hints

--- desk/datasheet/tests/test_forms.py
+++ desk/datasheet/tests/test_forms.py
@@ -5,6 +5,11 @@ from datasheet.forms import GuideForm
 class TestForm(SimpleTestCase):
 
     def test_form_valid_data(self) -> None:
+        """
+        Validate data
+        :return: Prepared data
+        :rtype: None
+        """
         form = GuideForm(data={
             'title': 'testTitleForm',
             'version': '0.1',
@@ -15,6 +20,11 @@ class TestForm(SimpleTestCase):
         self.assertTrue(form.is_valid())
 
     def test_form_no_data(self) -> None:
+        """
+        Valid form
+        :return: Valid form
+        :rtype: None
+        """
         form = GuideForm(data={})
 
         self.assertFalse(form.is_valid())
